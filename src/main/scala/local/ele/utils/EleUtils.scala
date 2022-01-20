63b20ca735424ea175ef60c1f6a30c60c08d8662
skokovin <s.kokovin@gmail.com>
2022-01-20 16:39:51 +0300

    kokovin release 13.11.17

--- src/main/scala/local/ele/utils/EleUtils.scala
+++ src/main/scala/local/ele/utils/EleUtils.scala
@@ -7,7 +7,7 @@ import java.sql.Statement
 
 object EleUtils {
 
-  private def sqlFixFbs(zoneName: String, systemName: String): String = s"update BS_DESIGN_NODE set \nMODEL_OID=\n(select M2 from \n        (select \n          DN.OID as DNOID,\n          DN.TYPE,\n          DN.MODEL_OID as M1,\n          PS.OID as M2,\n          " +
+  private def sqlFixFbsOld(zoneName: String, systemName: String): String = s"update BS_DESIGN_NODE set \nMODEL_OID=\n(select M2 from \n        (select \n          DN.OID as DNOID,\n          DN.TYPE,\n          DN.MODEL_OID as M1,\n          PS.OID as M2,\n          " +
     s"DN.NAME,\n          DN.DESCRIPTION,\n          PS.TYPE,\n          PS.ZONE,\n          PS.SYSTEM,\n          PS.LINE ,\n          PS.SQID,\n          PS.BDAPFIT   ,\n          PS.BDATRI,\n          PS.UUID,\n          PS.LINK_OID\n          " +
     s"from  BS_DESIGN_NODE DN ,PIPELINE_SEGMENT PS\n          where \n            PS.zone =(select seqid from zone where userid ='${zoneName}')  and \n            PS.system=(select seqid from SYSTEMS where name ='${systemName}') AND\n          " +
     s" (((PS.line||'-'||PS.line||'-'||PS.SQID) = DN.name) OR ((PS.line||'-'||PS.line||'-0'||PS.SQID) = DN.name))" +
@@ -16,6 +16,14 @@ object EleUtils {
     s"PS.zone =(select seqid from zone where userid ='${zoneName}')  and \n  PS.system=(select seqid from SYSTEMS where name ='${systemName}') AND\n  " +
     s"(((PS.line||'-'||PS.line||'-'||PS.SQID) = DN.name) OR ((PS.line||'-'||PS.line||'-0'||PS.SQID) = DN.name)) " +
     s"AND\n  DN.MODEL_OID <> PS.OID\n)"
+
+  private def sqlFixFbs(zoneName: String, systemName: String): String = s"update BS_DESIGN_NODE set \nMODEL_OID=\n(select M2 from \n        (select \n          DN.OID as DNOID,\n          DN.TYPE,\n          DN.MODEL_OID as M1,\n          " +
+    s"PS.OID as M2,\n          DN.NAME,\n          DN.DESCRIPTION,\n          PS.TYPE,\n          PS.ZONE,\n          PS.SYSTEM,\n          PS.LINE ,\n          PS.SQID,\n          PS.BDAPFIT   ,\n          PS.BDATRI,\n          " +
+    s"PS.UUID,\n          PS.LINK_OID\n          from  BS_DESIGN_NODE DN ,PIPELINE_SEGMENT PS\n          where \n            PS.zone =(select seqid from zone where userid in('${zoneName}'))  and \n            " +
+    s"PS.system=(select seqid from SYSTEMS where name in('${systemName}') ) AND\n            ((PS.line||'-'||PS.line||'-'||PS.SQID) = DN.name OR ((select name from zone where seqid=PS.zone)||'-'||PS.line||'-'||PS.SQID )= DN.name) \n          " +
+    s"AND\n          DN.MODEL_OID <> PS.OID\n        ) where DNOID = oid\n)\nwhere oid in (\nselect \n  DN.OID\n  from  BS_DESIGN_NODE DN ,PIPELINE_SEGMENT PS\n  where\n  PS.zone =(select seqid from zone where userid in('${zoneName}'))  and \n  " +
+    s"PS.system=(select seqid from SYSTEMS where name in('${systemName}') ) AND\n  " +
+    s"((PS.line||'-'||PS.line||'-'||PS.SQID) = DN.name OR ((select name from zone where seqid=PS.zone)||'-'||PS.line||'-'||PS.SQID )= DN.name   ) \n  AND\n  DN.MODEL_OID <> PS.OID\n)"
 
 
   def fixFBS(project: String, complectName: String): Unit = {
