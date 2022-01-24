5339ebeec454818e3470f5484880a6e6d2e96338
skokovin <s.kokovin@gmail.com>
2022-01-24 19:31:25 +0300

    kokovin release 13.11.33

--- src/main/scala/local/pdf/ru/ele/EleEqTrayESKDReport.scala
+++ src/main/scala/local/pdf/ru/ele/EleEqTrayESKDReport.scala
@@ -37,7 +37,7 @@ object EleEqTrayESKDReport extends Codecs with UtilsPDF {
     mmToPt(9 * kM),
     mmToPt(12 * kM),
     mmToPt(40 * kM),
-    mmToPt(109 * kM),
+    mmToPt(110 * kM),
     mmToPt(35 * kM),
     mmToPt(10 * kM),
     mmToPt(10 * kM),
@@ -45,7 +45,7 @@ object EleEqTrayESKDReport extends Codecs with UtilsPDF {
     mmToPt(10 * kM),
     mmToPt(15 * kM),
     mmToPt(10 * kM),
-    mmToPt(10 * kM) + 2
+    mmToPt(10 * kM)
   )
 
   def generateElecParts(project: String, complectName: String): List[Item11Columns] = {
