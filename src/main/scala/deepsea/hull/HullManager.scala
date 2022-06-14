45d1873bdddb65575d925ce85fbbe6007ce7c512
Bogdan Isaev <redeeming.fury@gmail.com>
2022-06-14 08:11:10 +0000

    some changes

--- src/main/scala/deepsea/hull/HullManager.scala
+++ src/main/scala/deepsea/hull/HullManager.scala
@@ -441,6 +441,7 @@ class HullManager extends Actor with Codecs{
             part.MATERIAL = value.MATERIAL
             part.STOCK_CODE = value.STOCK_CODE
             part.SECTION = value.THICK.toString
+            part.THICK = value.THICK
           case _ => None
         }
       }
