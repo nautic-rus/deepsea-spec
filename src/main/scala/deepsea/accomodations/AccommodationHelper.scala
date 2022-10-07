0cec6ebb0549b7f0032232ff2777aef7401ee204
bogdan <Whatab0utus>
2022-10-07 12:00:48 +0300

    some changes by bogdan

--- src/main/scala/deepsea/accomodations/AccommodationHelper.scala
+++ src/main/scala/deepsea/accomodations/AccommodationHelper.scala
@@ -213,7 +213,10 @@ trait AccommodationHelper {
         }
       case _ => List.empty[Accommodation]
     }
-    accommodations.map(_.asDevice).filter(m => m.material.code != "" && !groups.map(_.code).contains(m.material.code)).tapEach(x => x.units = "796").tapEach(x => x.material = x.material.copy(units = "796")).toList ++
+    accommodations.map(_.asDevice).filter(m => m.material.code != "" && !groups.map(_.code).contains(m.material.code)).tapEach(x => {
+      x.units = "796"
+      x.count = 1
+    }).toList ++
     accommodations.map(_.asDevice).filter(m => m.material.code != "" && groups.map(_.code).contains(m.material.code)).groupBy(_.material.code).map(acc => {
       acc._2.head.copy(weight = acc._2.map(_.weight).sum, count = acc._2.map(_.count).sum, userId = groups.find(_.code == acc._1) match {
         case Some(group) => group.userId
