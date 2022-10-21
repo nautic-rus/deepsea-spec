01a4b837979f12013baf4f6adaa1915696f40776
s.kokovin <s.kokovin@gmail.com>
2022-10-21 10:31:05 +0300

    some changes kokovin16

--- src/main/scala/local/eqmount/EqFoundationManager.scala
+++ src/main/scala/local/eqmount/EqFoundationManager.scala
@@ -12,7 +12,7 @@ import java.sql.{ResultSet, Statement}
 import java.util.Date
 import scala.collection.mutable.ListBuffer
 
-object EqFoundationManager  extends Codecs {
+object EqFoundationManager extends Codecs {
 
   case class EqFoundation(
                            BSOID: Int,
@@ -49,11 +49,14 @@ object EqFoundationManager  extends Codecs {
                            ZONENAME: String,
                            ZONEDESCR: String,
                            SYSTEMNAME: String,
-                           SYSTEMDESCR: String
+                           SYSTEMDESCR: String,
+                           HULLBLOCK: String = ""
                          )
 
 
-  private def foundationEqSql(): String = "  select \n  B.OID as BSOID,\n  B.NAME as BSNAME,\n  B.DESCRIPTION AS BSDESCRIPTION,\n  B.MOUNTING_SEQ_ORDER AS BSMOUNTING_SEQ_ORDER, \n  B.ADN AS BSADN,\n  (select NAME from BS_NODE where OID=B.PARENT_NODE)  AS BSFOUNDATION,\n  (select DESCRIPTION from BS_NODE where OID=B.PARENT_NODE)  AS BSFOUNDATION_DESCR,\n  E.OID as EOID,\n  E.TYPE as ETYPE,\n  E.USERID as EUSERID,\n  E.VERSION as EVERSION,\n  E.USER_NEW as EUSER_NEW,\n  E.DATE_NEW as EDATE_NEW,\n  E.USER_MOD as EUSER_MOD,\n  E.DATE_MOD as EDATE_MOD,\n  E.UUID as EUUID,\n  C.OID as COID,\n  C.TYPE as CTYPE,\n  C.ABBREV  as CABBREV,\n  C.ELEM_CLASS as CELEM_CLASS,\n  C.WEIGHT as CWEIGHT,\n  C.SUPLID as CSUPLID,\n  C.AC as CAC,\n  C.MT as CMT,\n  C.STOCK_CODE as CSTOCK_CODE,\n ELEMPOS.XPOS(E.OID,E.TYPE,E.DECK) as XPOS,\n ELEMPOS.YPOS(E.OID,E.TYPE,E.DECK) as YPOS,\n ELEMPOS.ZPOS(E.OID,E.TYPE,E.DECK) as ZPOS,\n ELEMPOS.XCOG(E.OID,E.TYPE,E.DECK,E.COMP) as XCOG,\n ELEMPOS.YCOG(E.OID,E.TYPE,E.DECK,E.COMP) as YCOG,\n ELEMPOS.ZCOG(E.OID,E.TYPE,E.DECK,E.COMP) as ZCOG, \n Z.NAME as ZONENAME,\n ZL.DESCR as ZONEDESCR,\n S.NAME as SYSTEMNAME,\n SL.DESCR as SYSTEMDESCR\n\n  from\n  (\n          select\n          OID,\n          NAME,\n          DESCRIPTION,\n          PARENT_NODE,\n          STD_LIBRARY,\n          STD_NAME,\n          ATOM_TYPE,\n          BS_NODE_TYPE_OID,\n          MOUNTING_SEQ_ORDER, \n          bs_node_adn_from_oid(OID) as ADN\n          from BS_NODE\n  ) \n  B, V_BS_NODE_DS_NODE DS, ELEMENT E, COMPONENT C, SYSTEMS S, SYSTEMS_LANG SL,\n  ZONE Z,ZONE_LANG ZL\n  where B.ADN like '%/Foundation/%' AND\n  B.ATOM_TYPE=3 AND\n  B.OID=NODE_OID AND\n  E.OID=DS.MODEL_OID AND\n  C.OID=E.COMP and\n  Z.OID=E.ZONE and\n  ZL.ZONE=Z.OID AND\n  ZL.LANG=-2 AND\n  S.OID=E.SYSTEM and\n  SL.SYSTEM=S.OID AND\n  SL.LANG=-2\n  "
+  private def foundationEqSql(): String = "select \n  B.OID as BSOID,\n  B.NAME as BSNAME,\n  B.DESCRIPTION AS BSDESCRIPTION,\n  B.MOUNTING_SEQ_ORDER AS BSMOUNTING_SEQ_ORDER, \n  B.ADN AS BSADN,\n  (select NAME from BS_NODE where OID=B.PARENT_NODE)  AS BSFOUNDATION,\n  (select DESCRIPTION from BS_NODE where OID=B.PARENT_NODE)  AS BSFOUNDATION_DESCR,\n  E.OID as EOID,\n  E.TYPE as ETYPE,\n  E.USERID as EUSERID,\n  E.VERSION as EVERSION,\n  E.USER_NEW as EUSER_NEW,\n  E.DATE_NEW as EDATE_NEW,\n  E.USER_MOD as EUSER_MOD,\n  E.DATE_MOD as EDATE_MOD,\n  E.UUID as EUUID,\n  C.OID as COID,\n  C.TYPE as CTYPE,\n  C.ABBREV  as CABBREV,\n  C.ELEM_CLASS as CELEM_CLASS,\n  C.WEIGHT as CWEIGHT,\n  C.SUPLID as CSUPLID,\n  C.AC as CAC,\n  C.MT as CMT,\n  C.STOCK_CODE as CSTOCK_CODE,\n ELEMPOS.XPOS(E.OID,E.TYPE,E.DECK) as XPOS,\n ELEMPOS.YPOS(E.OID,E.TYPE,E.DECK) as YPOS,\n ELEMPOS.ZPOS(E.OID,E.TYPE,E.DECK) as ZPOS,\n ELEMPOS.XCOG(E.OID,E.TYPE,E.DECK,E.COMP) as XCOG,\n ELEMPOS.YCOG(E.OID,E.TYPE,E.DECK,E.COMP) as YCOG,\n ELEMPOS.ZCOG(E.OID,E.TYPE,E.DECK,E.COMP) as ZCOG, \n Z.NAME as ZONENAME,\n ZL.DESCR as ZONEDESCR,\n S.NAME as SYSTEMNAME,\n SL.DESCR as SYSTEMDESCR\n\n  from\n  (\n          select\n          OID,\n          NAME,\n          DESCRIPTION,\n          PARENT_NODE,\n          STD_LIBRARY,\n          STD_NAME,\n          ATOM_TYPE,\n          BS_NODE_TYPE_OID,\n          MOUNTING_SEQ_ORDER, \n          bs_node_adn_from_oid(OID) as ADN\n          from BS_NODE\n  ) \n  B, V_BS_NODE_DS_NODE DS, ELEMENT E, COMPONENT C, SYSTEMS S, SYSTEMS_LANG SL,\n  ZONE Z,ZONE_LANG ZL\n  where B.ADN like '%/Foundation/%' AND\n  B.ATOM_TYPE=3 AND\n  B.OID=NODE_OID AND\n  E.OID=DS.MODEL_OID AND\n  C.OID=E.COMP and\n  Z.OID=E.ZONE and\n  ZL.ZONE=Z.OID AND\n  ZL.LANG=-2 AND\n  S.OID=E.SYSTEM and\n  SL.SYSTEM=S.OID AND\n  SL.LANG=-2\n  "
+
+  private def blocksSql(in: String) = s"select code, description from block where description in (${in})"
 
   def foranFoudationsAndEqs(project: String): List[EqFoundation] = {
     ConnectionManager.connectionByProject(project) match {
@@ -104,8 +107,34 @@ object EqFoundationManager  extends Codecs {
             )
           }
           stmt.close()
+
+          val sql2: String = {
+            var ts: String = ""
+            ret.groupBy(s => s.BSFOUNDATION).toList.foreach(t => {
+              ts += "'" + t._1 + "',"
+            })
+            blocksSql(ts.dropRight(1))
+          }
+          val stmt2: Statement = connection.createStatement()
+          val rs2: ResultSet = stmt2.executeQuery(sql2)
+          case class BlockFoundation(block: String, foundation: String)
+          val buffBlocks = ListBuffer.empty[BlockFoundation]
+          while (rs2.next()) {
+            buffBlocks += BlockFoundation(
+              Option(rs2.getString("code")).getOrElse(""),
+              Option(rs2.getString("description")).getOrElse("")
+            )
+          }
+          stmt2.close()
           connection.close()
-          ret.sortBy(f=>f.BSFOUNDATION).toList
+          val ret2 = ListBuffer.empty[EqFoundation]
+          ret.foreach(item => {
+            buffBlocks.find(s => s.foundation.trim.equals(item.BSFOUNDATION.trim)) match {
+              case Some(v) => ret2 += item.copy(HULLBLOCK = v.block)
+              case None => ret2 += item
+            }
+          })
+          ret2.sortBy(f => f.BSFOUNDATION).toList
         }
         catch {
           case e: Throwable =>
@@ -118,7 +147,7 @@ object EqFoundationManager  extends Codecs {
     }
   }
 
-  def foranFoudationsAndEqsJson(project: String):String=foranFoudationsAndEqs(project).asJson.noSpaces
+  def foranFoudationsAndEqsJson(project: String): String = foranFoudationsAndEqs(project).asJson.noSpaces
 
 
 }
