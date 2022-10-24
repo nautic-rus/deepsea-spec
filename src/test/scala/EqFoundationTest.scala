07c0be9a3d5523d0b7663880e53c285473093d37
bogdan <Whatab0utus>
2022-10-24 11:41:00 +0300

    some changes by bogdan

--- src/test/scala/EqFoundationTest.scala
+++ src/test/scala/EqFoundationTest.scala
@@ -4,6 +4,6 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class EqFoundationTest extends AnyFunSuite{
   val rets=foranFoudationsAndEqs("N004")
-  //val a=updateStatusSQL("N004",3401387,"Golenishev")
+  val a=updateStatusSQL("N004",3401387,"Golenishev")
 
 }
