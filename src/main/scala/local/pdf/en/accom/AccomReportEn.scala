2a4e4ef4583996f5df514c6c9071e6d3d075ea5f
bogdan <Whatab0utus>
2023-03-30 09:32:49 +0300

    some changes by bogdan

--- src/main/scala/local/pdf/en/accom/AccomReportEn.scala
+++ src/main/scala/local/pdf/en/accom/AccomReportEn.scala
@@ -586,7 +586,7 @@ object AccomReportEn extends UtilsPDF with DeviceHelper with MaterialsHelper {
       rawData.foreach(s => {
         if (s.userId.contains("*")){
           val nuid = s.userId.split("\\.").head
-          step1 += s.copy(userId = nuid, zone = "")
+          step1 += s.copy(userId = "", zone = "")
         }
         else if (s.userId.contains("#")) {
           val nuid = s.userId.split("#").head
@@ -634,7 +634,7 @@ object AccomReportEn extends UtilsPDF with DeviceHelper with MaterialsHelper {
       rows += Item11ColumnsEN(A1 = id, A2 = mat, A3 = unit, A4 = qty, A5 = weight, A6 = matDescr, A7 = room, A8 = drPos, A12 = row.material.code)
 
     })
-    rows.sortBy(s => s.A1).toList.sortBy(s => if (s.A1.contains(".")) s.A1.split("\\.").map(s => s.reverse.padTo(10 - s.length, '0').reverse).mkString("") else s.A1.reverse.padTo(10 - s.A1.length, '0').reverse)
+    rows.toList.sortBy(s => if (s.A1.contains(".")) s.A1.split("\\.").map(s => s.reverse.padTo(10 - s.length, '0').reverse).mkString("") else s.A1.reverse.padTo(10 - s.A1.length, '0').reverse)
   }
 
   private def genTotalRows(rawData: List[Device], lang: String, materials: List[Material]): List[Item11ColumnsEN] = {
