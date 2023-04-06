87a395f5c908b02fce2330bed5098fd1ba184097
bogdan <Whatab0utus>
2023-04-06 16:07:37 +0300

    some changes by bogdan

--- src/main/scala/deepsea/accomodations/AccommodationHelper.scala
+++ src/main/scala/deepsea/accomodations/AccommodationHelper.scala
@@ -260,7 +260,7 @@ trait AccommodationHelper {
         case _ => "NoUserId"
       }, material = if (acc._2.head.material.name.contains("L=")){
         acc._2.head.material.copy(name = acc._2.head.material.name + ", " + acc._2.length + "pcs",
-          translations = acc._2.head.material.translations.map(t => t.copy(name = acc._2.head.material.name + ", " + acc._2.length + "шт")))
+          translations = acc._2.head.material.translations.map(t => t.copy(name = t.name + ", " + acc._2.length + "шт")))
       }
       else{
         acc._2.head.material
@@ -276,7 +276,7 @@ trait AccommodationHelper {
         case _ => "NoUserId"
       }, material = if (acc._2.head.material.name.contains("L=")){
         acc._2.head.material.copy(name = acc._2.head.material.name + ", " + acc._2.length + "pcs",
-          translations = acc._2.head.material.translations.map(t => t.copy(name = acc._2.head.material.name + ", " + acc._2.length + "шт")))
+          translations = acc._2.head.material.translations.map(t => t.copy(name = t.name + ", " + acc._2.length + "шт")))
       }
       else{
         acc._2.head.material
