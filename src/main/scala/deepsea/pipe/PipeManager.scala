c05a43cd1c80a3d7d00df8d28bfedca181ea587c
bogdan <Whatab0utus>
2023-05-16 11:39:52 +0300

    some changes by bogdan

--- src/main/scala/deepsea/pipe/PipeManager.scala
+++ src/main/scala/deepsea/pipe/PipeManager.scala
@@ -187,7 +187,7 @@ class PipeManager extends Actor with Codecs with PipeHelper {
   implicit val timeout: Timeout = Timeout(60, TimeUnit.SECONDS)
 
   override def preStart(): Unit ={
-    self ! GetHvacSegs("200101-574-001")
+    self ! GetPipeSegs("N002", "721-004")
     //self ! GetSpoolModel("210101-545-0001", "032", "0")
   }
 
