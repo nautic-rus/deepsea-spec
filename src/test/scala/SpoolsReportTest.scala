39ecce6360d53b53a2ea33451da615027b965a5c
skokovin <s.kokovin@gmail.com>
2023-05-18 15:47:04 +0300

    kokovin94

--- src/test/scala/SpoolsReportTest.scala
+++ src/test/scala/SpoolsReportTest.scala
@@ -6,13 +6,13 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class SpoolsReportTest extends AnyFunSuite with PipeHelper {
 
-  val pipeSegs: List[PipeManager.PipeSeg] = getPipeSegsFromMongo("N004","210101-800-0001")
+  val pipeSegs: List[PipeManager.PipeSeg] = getPipeSegsFromMongo("N002","200101-721-004")
 //  val jk = pipeSegs.filter(_.spool == "197")
 //  val jkk = jk
 
-//  val ret: String = genSpoolsListEnPDF("210101-800-0001", "FUEL SYSTEM", "0", jk, "ru")
+  //val ret: String = genSpoolsListEnPDF("210101-800-0001", "FUEL SYSTEM", "0", jk, "ru")
 
-  val retAll: String = genSpoolsListEnPDFAll("210101-701-001", "FUEL SYSTEM", "0", pipeSegs,"en")
+  val retAll: String = genSpoolsListEnPDFAll("200101-721-004", "FUEL SYSTEM", "0", pipeSegs,"en")
 
  // println(ret)
   println(retAll)
