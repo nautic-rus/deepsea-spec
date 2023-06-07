ce9cc937d8f77f5128b69e45acf866611ca6badb
redwick <redeeming.fury@gmail.com>
2023-06-07 13:52:31 +0300

    some changes by bogdan

--- src/main/scala/deepsea/actors/ActorStartupManager.scala
+++ src/main/scala/deepsea/actors/ActorStartupManager.scala
@@ -29,14 +29,14 @@ class ActorStartupManager extends Actor{
     case DatabaseManagerStarted() =>
       ActorManager.httpServer = system.actorOf(RoundRobinPool(1).props(Props[HTTPManager]))
     case HTTPManagerStarted() =>
-      ActorManager.spec = system.actorOf(RoundRobinPool(10).props(Props[SpecManager]))
-      ActorManager.hullManager = system.actorOf(RoundRobinPool(10).props(Props[HullManager]))
-      ActorManager.elec = system.actorOf(RoundRobinPool(10).props(Props[ElecManager]))
+      ActorManager.spec = system.actorOf(RoundRobinPool(1).props(Props[SpecManager]))
+      ActorManager.hullManager = system.actorOf(RoundRobinPool(1).props(Props[HullManager]))
+      ActorManager.elec = system.actorOf(RoundRobinPool(1).props(Props[ElecManager]))
       ActorManager.files = system.actorOf(RoundRobinPool(3).props(Props[FileManager]))
       ActorManager.pipeCache = system.actorOf(RoundRobinPool(1).props(Props[PipeCache]))
-      ActorManager.pipe = system.actorOf(RoundRobinPool(5).props(Props[PipeManager]))
+      ActorManager.pipe = system.actorOf(RoundRobinPool(1).props(Props[PipeManager]))
       ActorManager.devices = system.actorOf(RoundRobinPool(1).props(Props[DeviceManager]))
       ActorManager.accommodations = system.actorOf(RoundRobinPool(1).props(Props[AccommodationManager]))
-      ActorManager.esp = system.actorOf(RoundRobinPool(3).props(Props[EspManager]))
+      ActorManager.esp = system.actorOf(RoundRobinPool(1).props(Props[EspManager]))
   }
 }
