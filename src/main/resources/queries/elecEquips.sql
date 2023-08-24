c7c069079d0116fd75e645417d78261deaa6bffd
redwick <redeeming.fury@gmail.com>
2023-08-24 10:26:17 +0300

    some changes by bogdan

--- src/main/resources/queries/elecEquips.sql
+++ src/main/resources/queries/elecEquips.sql
@@ -1,6 +1,7 @@
 select
     el.comp, el.userid, (select name from zone where oid = el.zone) as zone, cmp.stock_code
-from element el
-     left join component cmp on cmp.oid in (select comp from component_elec where mechanical = 0) and cmp.stock_code is not null
+from element el, component cmp
 where
-    el.system in (select oid from systems where name = '&system') and el.type = 10
+    el.system in (select oid from systems where name = '&system') and el.type = 10 and
+    el.COMP = cmp.OID and
+    cmp.oid in (select comp from component_elec where mechanical = 0) and cmp.stock_code is not null
