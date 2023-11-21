75985077b79573dd9b086bc4d91e067e20ba091d
redwick <redeeming.fury@gmail.com>
2023-11-21 13:51:34 +0300

    some changes by bogdan

--- src/main/scala/deepsea/esp/EspManager.scala
+++ src/main/scala/deepsea/esp/EspManager.scala
@@ -109,7 +109,7 @@ class EspManager extends Actor with EspManagerHelper with Codecs with PipeHelper
 //    val jk = qw
 //      self ! CreateEsp("N002", "200101-100-102", "0", "isaev", "device", 10330.toString)
    // val q = generateDeviceGlobalEsp(List("N002"))
-    self ! GetEsp("N002", "device", "200101-511-102")
+    //self ! GetEsp("N002", "device", "200101-511-102")
   }
   override def receive: Receive = {
     case CreateEsp(foranProject, docNumber, rev, user, kind, taskId) =>
