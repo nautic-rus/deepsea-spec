67ca4df0040f468b82381687150943c0d51590f4
redwick <redeeming.fury@gmail.com>
2024-04-03 15:47:21 +0300

    some changes

--- src/main/resources/queries/eleTrays.sql
+++ src/main/resources/queries/eleTrays.sql
@@ -1,4 +1,5 @@
 select * from pls_elem pls
 left join v_cabletray_stockcode vcs on vcs.system = pls.system and vcs.zone = pls.zone and vcs.line = pls.line and vcs.pls = pls.pls and vcs.elem = pls.elem
 left join element elm on elm.oid = pls.oid
+left join v_transit tr on tr.oid = pls.tray_fitting and tr.zone = pls.ZONE and tr.syst = pls.system
 where pls.system in (select seqid from systems where name in (&systemList))
