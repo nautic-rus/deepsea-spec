2daea06c2ee1a8a14a10866bbf9a6aaf4fc6ab3f
redwick <redeeming.fury@gmail.com>
2024-09-12 10:22:11 +0300

    some changes

--- src/main/scala/local/common/Codecs.scala
+++ src/main/scala/local/common/Codecs.scala
@@ -5,7 +5,7 @@ import deepsea.devices.DeviceManager.{Device, DeviceAux}
 import deepsea.elec.ElecManager.{CableBoxesBySystem, CableRoute, Cog, EleElement, ElecAngle, ElecCable, EquipmentConnection, TrayBySystem}
 import deepsea.esp.EspManager.{DeviceEspObject, DocumentWithMaterial, EleEspObject, EspElement, EspObject, GlobalEsp, GlobalEspSpec, HullEspObject, MaterialPurchase, PipeEspObject}
 import deepsea.hull.HullManager._
-import deepsea.pipe.PipeManager.{Material, MaterialTranslation, PipeSeg, PipeSegActual, PipeSegBilling, PipeSegExtended, ProjectName, SpecMaterial, SpoolLock, SystemDef, UnitTranslation, Units, UsrPar}
+import deepsea.pipe.PipeManager.{Material, MaterialTranslation, ParObj, PipeSeg, PipeSegActual, PipeSegBilling, PipeSegExtended, ProjectName, SpecMaterial, SpoolLock, SystemDef, UnitTranslation, Units, UsrPar}
 import io.circe.{Decoder, Encoder}
 import io.circe.parser._
 import io.circe.{Decoder, Encoder}
@@ -63,6 +63,9 @@ trait Codecs {
 
   implicit val UsrParDecoder: Decoder[UsrPar] = deriveDecoder[UsrPar]
   implicit val UsrParEncoder: Encoder[UsrPar] = deriveEncoder[UsrPar]
+
+  implicit val ParObjDecoder: Decoder[ParObj] = deriveDecoder[ParObj]
+  implicit val ParObjEncoder: Encoder[ParObj] = deriveEncoder[ParObj]
 
   implicit val PipeSegActualDecoder: Decoder[PipeSegActual] = deriveDecoder[PipeSegActual]
   implicit val PipeSegActualEncoder: Encoder[PipeSegActual] = deriveEncoder[PipeSegActual]
@@ -260,6 +263,7 @@ trait Codecs {
     classOf[PipeSeg],
     classOf[PipeSegExtended],
     classOf[UsrPar],
+    classOf[ParObj],
     classOf[PipeSegActual],
     classOf[Material],
     classOf[ProjectName],
